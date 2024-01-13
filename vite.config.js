9544cf936f38cfea00524ef0856165dec7db8e39
KINGAVIP <avipruthi8@gmail.com>
2024-01-13 14:04:54 +0530

    fix:Better API calls

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
-    strictPort: true,
-    port: 8000,
     proxy: {
-      "/api": "https://blogbackend-c8bk.onrender.com/",
+      "/api": "http://localhost:5000/",
+      "/auth": "http://localhost:5000/",
     },
   },
 });
